b1c702a983d22abe7edde5bd292337062140519b
goranp13 <goranp13@gmail.com>
2020-08-12 00:42:16 +0200

    xx
    xx

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log(window.document === document);
+console.log(window === self);
+console.log(document.lastModified);
+
+console.log(document.defaultView === window);
+console.log(document.title);
+
+console.log(document.body);
+console.log(document.captureEvents)
+
+document.body.innerHTML = "<h1>hey dom</h1>";
+
+//window.alert("hey dom");
+console.log(console);
