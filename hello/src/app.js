324f0d5f1f085af6a0f4e6c51f55a1e08123112f
goranp13 <goranp13@gmail.com>
2020-08-14 13:12:29 +0200

    mk

--- hello/src/app.js
+++ hello/src/app.js
@@ -3,4 +3,6 @@ let div = divs.item(0);
 let childNodes = div.childNodes; 
 let textNode = childNodes.item(0);
 console.log(textNode.getTextContent);
+let textInput = document.querySelector("input[type=text]");
+console.dir(textInput);
 
